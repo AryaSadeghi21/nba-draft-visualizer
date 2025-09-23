f671e9d910226aa3668d0edd2e58c020d1e0ec8a
Arya Sadeghi <Arya.sadeghi@scsstudent.org>
2025-09-23 14:28:26 -0700

    fixed file issue w ios app

---
+++ ios_app/Draft_PlaybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Draft_PlaybookApp.swift
+//  Draft_Playbook
+//
+//  Created by Arya Arya on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Draft_PlaybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios_app/Draft_PlaybookTests/Draft_PlaybookTests.swift
@@ -0,0 +1,17 @@
+//
+//  Draft_PlaybookTests.swift
+//  Draft_PlaybookTests
+//
+//  Created by Arya Arya on 9/22/25.
+//
+
+import Testing
+@testable import Draft_Playbook
+
+struct Draft_PlaybookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
